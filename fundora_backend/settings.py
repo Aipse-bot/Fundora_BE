4f30947b1f30f618038108bb1c2d9a55cdf3dc6a
Pauljesmarc <pjboyflores@gmail.com>
2026-03-04 22:11:28 +0800

    added Postgres DB

--- fundora_backend/settings.py
+++ fundora_backend/settings.py
@@ -151,11 +151,19 @@ WSGI_APPLICATION = 'fundora_backend.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),  # <- the env var name
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 # SSL configuration ONLY for PostgreSQL (when DATABASE_URL is set)
